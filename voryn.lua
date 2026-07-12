1dbc8b007c611cbdb2e47b77e67b4f475980638f
n0rixd3v <aymanmalik0884@gmail.com>
2026-07-12 23:05:39 +0530

    Add files via upload

---
+++ voryn.lua
@@ -0,0 +1,9 @@
+local FALLBACK_KEYS = {
+    "BBG-2011",   -- Owner
+    "SAGAR-2o5Q-PSsP-02Fo",   -- SagarSpider (sagarspider)
+    "SYKX0-93of-pd1l-M20v",   -- Ubramelech (sykx0_)
+    "AHMET-OLnv-03jo-m0K1",   -- S. (ahmet200robuxalcak_0)
+    "REVILO-ip5j-kOf6-02nc",   -- Revilo2.0 (revilo2.0)
+    "VAINS-20BX-0mxp-w1S3",   -- vains_ontop (vains_ontop)
+    "6QZ4-Yn3n-08kN-x7nD",   -- (6qz4)
+}
